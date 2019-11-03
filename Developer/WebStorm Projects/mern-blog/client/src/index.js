362fabbf98b89658c2ca72fd93454acafccd53e2
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-03 18:30:03 +0100

    add pages struct

--- Developer/WebStorm Projects/mern-blog/client/src/index.js
+++ Developer/WebStorm Projects/mern-blog/client/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'animate.css/animate.min.css';
 import './styles/global.scss';
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Root = () => (
-  <App />
+  <Router>
+      <App />
+  </Router>
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
