bb29938bf9ee27246edf122aadf00b3314409256
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-03 12:18:03 +0100

    project init

---
+++ Developer/WebStorm Projects/mern-blog/client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+            <ul>
+                {this.state.posts.map(post => <li key = {post.id}>{post.title}</li>)}
+            </ul>
+        </div>
+    );
+  }
+}
+
+state = {
+    posts: [],
+};
+
+componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+        .then(res => res.json())
+        .then(res => {
+            this.setState({ posts: res});
+        });
+}
+
+
+
+export default App;
