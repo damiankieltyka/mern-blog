e8b9b4dab84ad973e81b51ee3ea01de1e5ecd22b
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-03 19:08:03 +0100

    add sites

--- Developer/WebStorm Projects/mern-blog/client/src/App.js
+++ Developer/WebStorm Projects/mern-blog/client/src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import MainLayout from './components/layout/MainLayout/MainLayout';
+import MainLayout from '../components/layout/MainLayout/MainLayout';
 
 import Home from './components/pages/Home/HomePage';
 import Posts from './components/pages/Posts/PostsPage';
 import Contact from './components/pages/Contact/ContactPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
 
-state = {
-    posts: [],
-};
-
 class App extends React.Component {
-  render() {
-    return (
-        <MainLayout>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/posts" exact component={Posts} />
-                <Route path="/contact" exact component={Contact} />
-                <Route component={NotFound} />
-            </Switch>
-        </MainLayout>
-    );
-  }
+
+    render() {
+        return (
+            <MainLayout>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/posts" exact component={Posts} />
+                    <Route path="/contact" exact component={Contact} />
+                    <Route component={NotFound} />
+                </Switch>
+            </MainLayout>
+        );
+    }
 }
 
 export default App;
