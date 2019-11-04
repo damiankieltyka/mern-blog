499f2e8bafee56060eab5d10073e196020ccd301
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-05 00:08:57 +0100

    install thunk

---
+++ Developer/WebStorm Projects/mern-blog/client/src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+import posts from './postsRedux';
+
+const rootReducer = combineReducers({
+    posts,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
