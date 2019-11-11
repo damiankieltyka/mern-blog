9a1a6daf4ce6a7a980d361c0a2d030f671511d4c
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-11 21:43:08 +0100

    add MainLayout

--- Developer/WebStorm Projects/mern-blog/client/src/config.js
+++ Developer/WebStorm Projects/mern-blog/client/src/config.js
@@ -1,16 +1,1 @@
-import { API_URL } from '../config';
-
-export const loadPostsRequest = () => {
-    return dispatch => {
-
-        axios.get(`${API_URL}/posts`).then(res => {
-            dispatch(loadPosts(res.data));
-        })
-            .catch(err => {
-                console.log(err);
-            });
-
-    };
-};
-
 export const API_URL = 'http://localhost:8000/api';
