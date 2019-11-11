b30b632b80edb4e63032d920eb0e68aaf9d16a9a
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-11 17:50:27 +0100

    add post list

--- Developer/WebStorm Projects/mern-blog/client/src/redux/store.js
+++ Developer/WebStorm Projects/mern-blog/client/src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import posts from './postsRedux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     posts,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
