c8c347d0ec3edd22b3de05b882653039bf5f0dca
damiankieltyka <damian.kieltyka@icloud.com>
2019-11-12 22:49:03 +0100

    add mongo

---
+++ Developer/WebStorm Projects/mern-blog/server/routes/post.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const PostController = require('../controllers/post.controller');
+
+router.route('/posts').get(PostController.getPosts);
+
+module.exports = router;
